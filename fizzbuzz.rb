352e585a213e1a4aaf6ca2e95aa759c253d813bb
Jacob G. Wazydrag <jgw.jakegeorge@gmail.com>
2020-04-30 12:18:46 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,14 +1,13 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-require 'rspec'
-def fizzbuzz(number)
-  if number % 3 == 0 && number % 5 == 0
-    "FizzBuzz"
-  elsif number % 3 == 0
-    "Fizz"
-  elsif number % 5 == 0
-    "Buzz"
+def fizzbuzz(num)
+  if (num % 5 == 0) && (num % 3 == 0)
+    return "FizzBuzz"
+  elsif num % 3 == 0
+    return "Fizz"
+  elsif num % 5 == 0
+    return "Buzz"
   else
-    number
+    return
   end
 end
